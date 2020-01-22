3194ec7562234c2cb0583c186b8e863917390d5d
unknown <austin.l.killian@gmail.com>
2020-01-22 16:00:45 -0700

    more material UI figured out, styling done, intro section added

--- src/App.js
+++ src/App.js
@@ -1,36 +1,59 @@
 import React from "react";
 import "./App.scss";
 import "./reset.scss";
+import "./type.scss";
 import CssBaseline from "@material-ui/core/CssBaseline";
 import Grid from "@material-ui/core/Grid";
+import Container from "@material-ui/core/Container";
+import Box from "@material-ui/core/Box";
+import { shadows } from "@material-ui/system";
 
 function App() {
-
   return (
     <React.Fragment>
       <CssBaseline />
 
       <div className="main home">
         <header className="hero">
-          <div>
-            <h1>MY NAME IS AUSTIN KILLIAN</h1>
+          <Container maxWidth="md">
+            <div>
+              <h1>
+                I'M
+                <br />
+                AUSTIN KILLIAN
+              </h1>
 
-            <h2>hear me roar</h2>
-          </div>
+              <h2>WEB DEVELOPER</h2>
+
+              <h3>WITH AN EDGE</h3>
+            </div>
+          </Container>
         </header>
 
-        <section className="intro">
-          <h2>
-            I believe in creativity and applying it to everything that you do.
-          </h2>
+        <section className="statement">
+          <Container>
+            <h1>MAKE YOUR MARK</h1>
+          </Container>
+        </section>
 
-          <Grid container>
-            <Grid item xs={12}>
-              <div className="item">
-                <h4>MAKE YOUR MARK</h4>
-              </div>
-            </Grid>
-          </Grid>
+        <section className="intro">
+          <Container maxWidth="md">
+            <div className="inner-container">
+              <Grid container spacing={3}>
+                <Grid item xs={12}>
+                  <div className="profile-img-container">
+                    <div className="profile-img"></div>
+                  </div>
+                </Grid>
+
+                <Grid item xs={12}>
+                  <p>
+                    I work hard to keep creativity alive in all aspects of my life and the lives of those around me. My passion is creation. Whether I'm at work or at home, my mind is always thinking of the next best idea for the project I'm working on and I won't stop until it is great.
+                  </p>
+                </Grid>
+              </Grid>
+            </div>
+          </Container>
         </section>
       </div>
     </React.Fragment>
